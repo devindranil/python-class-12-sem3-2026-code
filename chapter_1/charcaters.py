11c69eff93678729ae043244f31910925ba062f5
learnshyindranil <ic880572@gmail.com>
2026-04-07 09:55:10 +0530

    first commit

---
+++ chapter_1/charcaters.py
@@ -0,0 +1,42 @@
+# letters
+name = "Virat Kohli"
+# digit
+#age = 38
+# special symbols
+# + = addition operator/concatenation operator
+# string concatenation
+# 5 + 10 = addition operator
+# white space - \n = new line, \t=tab line
+message = "\t\tHello, \n" + name + "\n" + "I am former Indian Team Captain..."
+
+
+#print(name)
+#print(age)
+#print(message)
+
+name1 = "Megha Das" #string using double quotation
+name2 = 'Sayak Goswami' #single quotation
+'''message2 = "Stay afraid, but do it anyway." \
+" What’s important is the action. " \
+"You don’t have to wait to be confident. " \
+"Just do it and eventually the confidence will follow.” - Carrie Fisher"'''
+
+# multi line string - to format the string more better way
+message2='''
+-----------Todays Motivational Quotes-------------
+Stay afraid, but do it anyway. 
+    What’s important is the action. 
+You don’t have to wait to be confident. Just do it and eventually the confidence will follow.” - Carrie Fisher
+--------------------------------------------------
+'''
+
+'''print(name1)
+print(name2)
+print(message2)'''
+
+# when you only define the variable name but not assign any value inside the variable name that time you got an NameError.
+# None Literals
+'''age=None #use to represent the absense of the value
+print(age)'''
+
+print("Hello" "World" "I am Indranil") #adjacent string automatically concatenate
---
+++ chapter_1/main.py
@@ -0,0 +1,51 @@
+# variable declaration - single line comment
+'''
+--- Multi Line comments in Python
+--- variable declaration syntax-----
+name of variable = value / data
+LHS (L-Value) = RHS (R-Value)
+L-value = memory location or variable name
+R-value = data or value 
+= assignment operator which help us to assign the value inside the variable memory
+'''
+number = 25 #integer type data
+pi = 3.14 #float data type
+name = "Indranil Chakraborty" #string type data
+print(number)
+print(pi)
+
+#accessing the memory of variable - print the value of variable
+'''print(number)
+print("The value of pi is: ",pi)
+print("My name is: ",name)'''
+
+#Re-declaration of same variable
+'''number = 55
+pi=4.5
+print(number)
+print(pi)'''
+
+# valid rules of python variables
+name = "Indranil" 
+NAME = "Sayak" #variable name in python are case sensitive
+Name = "Eshita"
+_name = "Megha das"
+name01 = "Priyanka Ghosh"
+firstName = "Shreya"
+lastName = "Ghosh"
+book_price_value = 205
+
+print(name,NAME,_name,firstName,lastName,name01,book_price_value,Name)
+
+# invalid rules of python variables name
+'''1name
+first name
+if #keyword
+while #keyword
+for #keyword
+name@$$^ff
+@name
+hjkd$
+first-name
+first+/*name'''
+
