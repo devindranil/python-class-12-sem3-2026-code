5a82a4aa0681eb26222db8ecd4aac39c42a42091
learnshyindranil <ic880572@gmail.com>
2026-05-19 09:32:27 +0530

    update

---
+++ errors/main.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("Enter two numbers: \n");
+    scanf("%d%d",&a,&b);
+    int res=a+b;
+    printf("The output is: %d",res);
+    return 0;
+}
---
+++ errors/main.py
@@ -0,0 +1,26 @@
+a=int(input("Enter the value of a: "))
+b=int(input("Enter the value of b: "))
+print("The program is compiled")
+
+
+res=a/b #20/4=5.0 ZeroDivisionError
+
+#IndexError
+n=[10,20,30]
+#print(n[4]) 
+n.append(30)
+print(n)
+
+# AttributeError:
+m=10 #m is just an integer variable
+#m.append(20) #it's always accept the list value not any int obj
+print(m)
+
+#ValueError
+x=int(input("Enter any value: "))
+print(x)
+
+
+
+#print(ress) #NameError
+
